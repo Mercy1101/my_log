8445a268cb14a8281fc296c9b1e1a860fa16e8f1
lijiancong <18221336269@163.com>
2020-07-18 15:30:24 +0800

    工程框架建立起来了

--- src/main.cc
+++ src/main.cc
@@ -1,9 +1,13 @@
+/// Copyright (c) 2019,2020 Lijiancong. All rights reserved.
+///
+/// Use of this source code is governed by a MIT license
+/// that can be found in the License file.
+
 #define CATCH_CONFIG_RUNNER
 #include <catch2/catch.hpp>
 #include <iostream>
 
 int main(int argc, char** argv) {
   Catch::Session().run(argc, argv);
-
   return 0;
 }
