647b230143be356aca9b5c7b045d1e0346ae3d65
lijiancong <18221336269@163.com>
2020-07-18 16:20:47 +0800

    文件格式整理

---
+++ include/log.hpp
@@ -0,0 +1,17 @@
+///////// ///////// ///////// ///////// ///////// ///////// ///////// /////////
+/// Copyright (c) 2019,2020 Lijiancong. All rights reserved.
+///
+/// Use of this source code is governed by a MIT license
+/// that can be found in the License file.
+///
+/// @file   log.hpp
+/// @brief  日志文件
+///
+/// @author lijiancong, pipinstall@163.com
+/// @date   2020-07-18 11:39:27
+///////// ///////// ///////// ///////// ///////// ///////// ///////// /////////
+
+#ifndef MY_LOG_INCLUDE_LOG_H_
+#define MY_LOG_INCLUDE_LOG_H_
+
+#endif  // end of MY_LOG_INCLUDE_LOG_H_
---
+++ include/queue.hpp
@@ -0,0 +1,17 @@
+///////// ///////// ///////// ///////// ///////// ///////// ///////// /////////
+/// Copyright (c) 2019,2020 Lijiancong. All rights reserved.
+///
+/// Use of this source code is governed by a MIT license
+/// that can be found in the License file.
+///
+/// @file   queue.hpp
+/// @brief  日志消息队列的函数
+///
+/// @author lijiancong, pipinstall@163.com
+/// @date   2020-07-18 15:32:40
+///////// ///////// ///////// ///////// ///////// ///////// ///////// /////////
+
+#ifndef MY_LOG_INCLUDE_QUEUE_H_
+#define MY_LOG_INCLUDE_QUEUE_H_
+
+#endif  // end of MY_LOG_INCLUDE_QUEUE_H_
